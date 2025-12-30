554616090dca4871e1a1511dcb7351d96f5fffa7
ABHISHEK SINGH <87164587+abhisheksingh-20@users.noreply.github.com>
2025-12-30 15:35:27 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: true,
+    origin: "https://chat-application-abhisheksingh-20s-projects.vercel.app",
     credentials: true,
   })
 );
